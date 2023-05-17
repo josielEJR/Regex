0ae41fb5e73950bd526164adca60632d771924a2
josielEJR <125618119+josielEJR@users.noreply.github.com>
2023-05-17 11:11:57 -0300

    Update validação-de-email.js

--- Projeto/validação-de-email.js
+++ Projeto/validação-de-email.js
@@ -1,9 +1,9 @@
 const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
-console.log('foo.bar@gmail.com =>', emailRegex.test('foo.bar@gmail.com'))
-console.log('foo.bar@gmail.com.br =>', emailRegex.test('foo.bar@gmail.com.br'))
-console.log('foo.bar@gmail.com.br.br =>', emailRegex.test('foo.bar@gmail.com.br.br'))
-console.log('foo.bar@gmail. =>', emailRegex.test('foo.bar@gmail.'))
-console.log('foo.bar@gmailcom =>', emailRegex.test('foo.bar@gmailcom'))
-console.log('foo.bargmail.com =>', emailRegex.test('foo.bargmail.com'))
-console.log('@gmail.com =>', emailRegex.test('@gmail.com'))
+console.log('foo.bar@gmail.com =>', emailRegex.test('foo.bar@gmail.com')) //true
+console.log('foo.bar@gmail.com.br =>', emailRegex.test('foo.bar@gmail.com.br')) //true
+console.log('foo.bar@gmail.com.br.br =>', emailRegex.test('foo.bar@gmail.com.br.br'))  //true
+console.log('foo.bar@gmail. =>', emailRegex.test('foo.bar@gmail.')) //false
+console.log('foo.bar@gmailcom =>', emailRegex.test('foo.bar@gmailcom')) //false
+console.log('foo.bargmail.com =>', emailRegex.test('foo.bargmail.com')) //false
+console.log('@gmail.com =>', emailRegex.test('@gmail.com')) //false
 
